0e7f5942f94b0feb83cda051232ae796419bf050
ale-gen <o.generowicz@gmail.com>
2022-02-18 11:52:37 +0100

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,20 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Aleksandra Generowicz on 18/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
