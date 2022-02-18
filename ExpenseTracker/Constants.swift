41671a8413092159d2bd88846121e156e94b6079
ale-gen <o.generowicz@gmail.com>
2022-02-18 12:51:55 +0100

    Add constants file

---
+++ ExpenseTracker/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  ExpenseTracker
+//
+//  Created by Aleksandra Generowicz on 18/02/2022.
+//
+
+import Foundation
+import SwiftUI
+
+struct K {
+    static let tabItemLabels = ["Trips", "Charts", "", "Groups", "Search"]
+    static let tabItemImages = ["case.fill", "chart.bar.xaxis", "plus", "list.bullet", "magnifyingglass"]
+    static let monthsTable = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Nov", "Dec"]
+    
+    static let borderColor = Color(red: 120/255, green: 122/255, blue: 145/255, opacity: 1)
+    static let customNavyColor = Color(red: 20/255, green: 39/255, blue: 155/255, opacity: 1)
+    static let customOrangeColor = Color(red: 1, green: 130/255, blue: 67/255, opacity: 1)
+}
