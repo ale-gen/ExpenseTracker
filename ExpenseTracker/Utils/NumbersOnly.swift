5442bb9ae2ade3916d57e0a3de5e332fc27b4211
ale-gen <o.generowicz@gmail.com>
2022-03-14 17:33:35 +0100

    Fix entering commas and dots in expense amount textfield

--- ExpenseTracker/Utils/NumbersOnly.swift
+++ ExpenseTracker/Utils/NumbersOnly.swift
@@ -9,11 +9,21 @@ import Foundation
 
 struct NumbersOnly {
     static func filterNumbers(for input: String) -> String {
+        let availablePunctuation = [Character("."), Character(",")]
         var result: String
-        let filteredData = input.filter { $0.isNumber }
-        
-        if input != filteredData {
-            result = filteredData
+        let filteredData = input.filter { $0.isNumber || availablePunctuation.contains($0) }
+        let reducedData = filteredData.reduce("") { partialResult, char in
+            if !char.isNumber && !(partialResult.contains(availablePunctuation[0]) || partialResult.contains(availablePunctuation[1])) {
+                return "\(partialResult)\(char)"
+            } else if char.isNumber {
+                return "\(partialResult)\(char)"
+            } else {
+                return partialResult
+            }
+        }
+        //TODO: - Fix decimal format - 2 digit after commas or dot
+        if input != reducedData {
+            result = reducedData
         } else {
             result = input
         }
