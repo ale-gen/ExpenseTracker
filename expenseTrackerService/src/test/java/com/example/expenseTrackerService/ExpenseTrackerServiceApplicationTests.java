58d0b5cfdf80090923f63219eefbac3aeed25d0d
ale-gen <o.generowicz@gmail.com>
2022-04-21 00:39:20 +0200

    Add initial SpringBoot project

---
+++ expenseTrackerService/src/test/java/com/example/expenseTrackerService/ExpenseTrackerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expenseTrackerService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
